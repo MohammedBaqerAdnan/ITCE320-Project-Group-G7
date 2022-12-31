9d224fcd5a0e0acef561f45c07c107ee785f3797
Mohammed Baqer Adnan <sayedmohammedadnanbiz@gmail.com>
2022-12-31 14:31:47 +0300

    Edit search functions, handle connection functions, and add sample file for testing server without client

--- Server/Test_Server_without_client.py
+++ Server/Test_Server_without_client.py
@@ -1,31 +1,111 @@
-import json,threading,socket,requests
+'''import requests , json
 
-#main function with try and except and create a thread to handle the client
-def main():
-    try:
-        # Accept a connection
-        (active_socket, client_address) = accept_connection(s)
-        #create a thread to handle the client
-        t=threading.Thread(target=handle_client_request,args=(active_socket,client_address))
-        t.start()
-    except Exception as e:
-        print(e)
-        return False
+params = {
+    'access_key': '758fff0b507ff10b8b758af057976ab6',
+    'limit' : '2'
+}
 
-#function to retrieve flight information
-def flight_info(airport_code):
-    key='a499d3ebda4cd62efd7841c09e4e43bf'
-    parameter={'access_key':key,'limit':100,'arr_icao':airport_code}
-    response=requests.get('http://api.aviationstack.com/v1/flights',parameter)
-    response=response.json()
-    return response
+api_result = requests.get('http://api.aviationstack.com/v1/flights', params).json()
 
+dat = json.dumps(api_result)
+
+data = json.loads(dat)
+
+with open('groupe_15.json' , 'w') as file :
+    json.dump(api_result , indent= 3 , fp= file ) '''
+
+
+import requests,json ,sys
+
+flight_info={
+    "pagination": {
+        "limit": 100,
+        "offset": 0,
+        "count": 100,
+        "total": 1669022
+    },
+    "data": [
+        {
+            "flight_date": "2019-12-12",
+            "flight_status": "active",
+            "departure": {
+                "airport": "San Francisco International",
+                "timezone": "America/Los_Angeles",
+                "iata": "SFO",
+                "icao": "KSFO",
+                "terminal": "2",
+                "gate": "D11",
+                "delay": 13,
+                "scheduled": "2019-12-12T04:20:00+00:00",
+                "estimated": "2019-12-12T04:20:00+00:00",
+                "actual": "2019-12-12T04:20:13+00:00",
+                "estimated_runway": "2019-12-12T04:20:13+00:00",
+                "actual_runway": "2019-12-12T04:20:13+00:00"
+            },
+            "arrival": {
+                "airport": "Dallas/Fort Worth International",
+                "timezone": "America/Chicago",
+                "iata": "DFW",
+                "icao": "KDFW",
+                "terminal": "A",
+                "gate": "A22",
+                "baggage": "A17",
+                "delay": 0,
+                "scheduled": "2019-12-12T04:20:00+00:00",
+                "estimated": "2019-12-12T04:20:00+00:00",
+                "actual": None,
+                "estimated_runway": None,
+                "actual_runway": None
+            },
+            "airline": {
+                "name": "American Airlines",
+                "iata": "AA",
+                "icao": "AAL"
+            },
+            "flight": {
+                "number": "1004",
+                "iata": "AA1004",
+                "icao": "AAL1004",
+                "codeshared": None
+            },
+            "aircraft": {
+               "registration": "N160AN",
+               "iata": "A321",
+               "icao": "A321",
+               "icao24": "A0F1BB"
+            },
+            "live": {
+                "updated": "2019-12-12T10:00:00+00:00",
+                "latitude": 36.28560000,
+                "longitude": -106.80700000,
+                "altitude": 8846.820,
+                "direction": 114.340,
+                "speed_horizontal": 894.348,
+                "speed_vertical": 1.188,
+                "is_ground": False
+            }
+        }, 
+    ]
+}
 #function Store the all flight information data in a json file
 def store_flight_info(flight_info):
     #open the json file
     with open('group_7.json','w') as file:
         #store the flight information in json file
         json.dump(flight_info,file,indent=2)
+
+#function search if the option is 1 or 2 or 3 or 4 then call the function to store the flight information in json object
+def search_flight(option,value_search,flight_info):
+
+    if option=='1':
+        re_flight_info=arrive_flight_info(flight_info)
+    elif option=='2':
+        re_flight_info=delayed_flight_info(flight_info)
+    elif option=='3':
+        re_flight_info=city_flight_info(flight_info,value_search)
+    elif option=='4':
+        re_flight_info=flight_details(flight_info,value_search)
+    return re_flight_info
 
 #function to store the flight arrive (flight information: flight number and estimated time of arrival and  terminal number) in json object 
 def arrive_flight_info(flight_info):
@@ -105,27 +185,11 @@ def flight_details(flight_info,flight_number):
             flight_city_info['delay'].append(flight['arrival']['delay'])
     return flight_city_info
 
-#function search if the option is 1 or 2 or 3 or 4 then call the function to store the flight information in json object
-def search_flight(option,value_search,flight_info):
-
-    if option=='1':
-        re_flight_info=arrive_flight_info(flight_info)
-    elif option=='2':
-        re_flight_info=delayed_flight_info(flight_info)
-    elif option=='3':
-        re_flight_info=city_flight_info(flight_info,value_search)
-    elif option=='4':
-        re_flight_info=flight_details(flight_info,value_search)
-    return re_flight_info
-
 #create a list to store the online clients 
 online_clients=[]
 #function to handle the client request Wait for clients' requests to connect (should be able to accept three connections simultaneously).Accept the connection and Store the client’s name and display it on the terminal.
-def handle_client_request(active_socket,id_number):
+def handle_client_request(id_number,client_name,airport_code,client_option):
     try:
-        # Receive the client's name
-        client_name = active_socket.recv(1024).decode()
-        #create a json object to store the client information his name and id number
         client_info={
             'name':client_name,
             'id':id_number
@@ -133,65 +197,26 @@ def handle_client_request(active_socket,id_number):
         online_clients.append(client_info)
         # Display the client's name and id with the message "has connected"
         print(f'client {client_name} with id {id_number} has been connected')
-        #receive the airport code from the client
-        airport_code=active_socket.recv(1024).decode()
-        #call the function to get the flight information from the airport code
-        flight_info=flight_info(airport_code)
         #call the function store the flight information in json file
         store_flight_info(flight_info)
         while True:
-            # Receive the client's option with value_search in json object
-            client_option = active_socket.recv(1024).decode()
-            #convert the json object to python dictionary
-            client_option=json.loads(client_option)
             #if the client enter exit option then break the loop
             if client_option['option']=='exit':
                 break
             #call the function to search the flight information
             flight_info_in=search_flight(client_option['option'],client_option['value_search'],flight_info)
-            #convert the flight information to json object  to send it to the client
-            flight_info_in=json.dumps(flight_info)
-            # Send the flight information to the client
-            active_socket.sendall(flight_info_in.encode())
+            break
         # Display the client's name and id with the message "has disconnected"
+        print(flight_info_in)
         print(f'client {client_name} with id {id_number} has been disconnected')
         #remove the client from the online clients list
         online_clients.remove(client_info)
-    except:
+    except Exception as e:
+        print(e)
+        #diplay the error line number
+        print(sys.exc_info()[-1].tb_lineno)
         print(f'client {client_name} with id {id_number} has been disconnected')
         #remove the client from the online clients list
         online_clients.remove(client_info)
 
-
-
-
-#function to create a passive socket
-def create_passive_socket(listen_backlog):
-    # Create a socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Bind the socket to a port
-    s.bind(('  ', 8999))
-    # Listen for connections
-    s.listen(listen_backlog)
-    # Return the socket and the port number
-    return s
-
-#function to accept a connection
-def accept_connection(s):
-    # Accept a connection
-    (active_socket, client_address) = s.accept()
-    # Return the socket and the client address
-    return (active_socket, client_address)
-
-
-# Create a passive socket
-s = create_passive_socket(3)
-
-#call the main function in while loop
-while True:
-    #if the main function return false break the loop
-    if main()==False:
-        break
-
-#close socket
-s.close()
+handle_client_request(1,'ahmed','CAI',{"option":'2',"value_search":'AAL1004'})
