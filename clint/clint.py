9681d7e53993f108931d40375e615c7ca5d70faf
Mohammed Baqer Adnan <sayedmohammedadnanbiz@gmail.com>
2022-12-31 15:11:28 +0300

    Editing the sending function variables and indentation

--- clint/clint.py
+++ clint/clint.py
@@ -1,51 +1,59 @@
-import socket
-import json
-import tkinter
+import socket , json , tkinter
 
-#creatig the socket
+#creating the socket
 #port=8999
 with socket.socket(socket.AF_INET , socket.SOCK_STREAM) as cs:
-        cs.connect(("127.0.0.1" , 8999))
+    cs.connect(("127.0.0.1" , 8999))
 
-        #creating a top level window
-        top = tkinter.Tk()
-        top.mainloop()
+    #creating a top level window
+    top = tkinter.Tk()
+    top.mainloop()
 
 
+    
+    #sending the name and airport NO
+    def information():
+        #ask the user to enter his name and the airport NO
+        name = input("Enter your name: ")
+        airport = input("Enter the airport NO: ")
+        cs.send(name.encode())
+        cs.send(airport.encode())
+
+    #function of arrival flights
+    def arrival_flight() :
+        #creating variable a dictionary to send the data with the option number and the value of the search
+        choice = {"option":'1',"value_search":None}
+        choice = json.dumps(choice)
+        cs.send(choice.encode())
+        data , address = cs.recv(19999)   
+        d = json.loads(data)
+    
         
-        #sending the name and airport NO
-        def information():
-            name = input("pleas enter your name")
-            airPCode = input("pleas enter the airporrt NO")
-            cs.send(name.encode())
-            cs.send(airPCode.encde())
+    #function of delayed flights            
+    def delayed() :
+        choice = {"option":'2',"value_search":None}
+        choice = json.dumps(choice)
+        cs.send(choice.encode())
+        data , address = cs.recv(19999)   
+        d = json.loads(data)
 
-        #function of arrival flights
-        def arrival_flight() :
-            cs.send('1'.encode())
-            data , address = cs.recv(19999)   
-            d = json.loads(data)
-        
-         
-        #function of delayed flights            
-        def delayed() :
-            cs.send('2'.encode())
-            data , address = cs.recv(19999)   
-            d = json.loads(data)
+    #function of city information
+    def City_info():
+        City_name = input("Enter the name of the city you are looking for: ")
+        choice = {"option":'3',"value_search":City_name}
+        choice = json.dumps(choice)
+        cs.send(choice.encode())
+        data = cs.recv(19999)            
+        d  = json.loads(data)
 
-        #function of city informations
-        def City_info():
-            cs.send('3'.encode())
-            City_name = input("pleas enter the name of the city you are looking for: ")
-            data = cs.recv(19999)            
-            d  = json.loads(data)
-
-        #function of flight details
-        def specfic_flight():
-            cs.send('4'.encode())  
-            flight_NO = input("pleas enter the flight number: ")   
-            data = cs.recv(19999)  
-            d  = json.loads(data)
+    #function of flight details
+    def specific_flight():
+        flight_NO = input("Enter the flight number: ")   
+        choice = {"option":'4',"value_search":flight_NO}
+        choice = json.dumps(choice)
+        cs.send(choice.encode())
+        data = cs.recv(19999)  
+        d  = json.loads(data)
 
 
 
